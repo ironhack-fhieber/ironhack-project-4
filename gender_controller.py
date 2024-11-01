d4f8112889d2e8c16b13a78491d6167e4e6517ff
Fabian Hieber <hibi1337@gmail.com>
2024-11-01 20:12:03 +0100

    revert torch removal

--- gender_controller.py
+++ gender_controller.py
@@ -1,19 +1,18 @@
-import numpy as np
+import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 classifier = AutoModelForSequenceClassification.from_pretrained("padmajabfrl/Gender-Classification")
-classifier.to('cpu')
+classifier.to(device)
 tokenizer = AutoTokenizer.from_pretrained("padmajabfrl/Gender-Classification")
 
 
 def get_gender(name):
-    # Tokenisation
-    tokens = tokenizer(name, return_tensors="pt").to('cpu')
+    # Tokenisation and shortening to the maximum length
+    tokens = tokenizer(name, return_tensors="pt").to(device)
+
+    # Perform sentiment analysis for the shortened text
     logits = classifier(**tokens).logits
-
-    # Convert logits to a NumPy array
-    logits_np = logits.detach().numpy()
-
-    # Get the predicted index using NumPy's argmax
-    predicted_index = np.argmax(logits_np, axis=1)[0]
+    predicted_index = torch.argmax(logits, dim=1).item()
     return {0: 'm', 1: 'f'}[predicted_index]
